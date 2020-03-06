a0ffc6c3dd30bbe06b8d3b8aacf0ddb9c03e7273
Alexander Bachschmid <alexander.bachschmid@prochannel.eu>
2020-03-06 13:46:59 +0100

    German Translation

---
+++ resources/lang/de/verification.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'verified' => 'Ihre E-Mail wurde verifiziert!',
+    'invalid' => 'Dieser Verifikations-Link ist leider ungültig.',
+    'already_verified' => 'Ihre E-Mail wurde bereits verifiziert.',
+    'user' => 'Wir können keinen Benutzer mit dieser E-Mail Adresse finden.',
+    'sent' => 'Wir haben Ihnen soeben eine E-Mail mit dem Verifikations-Link gesendet.',
+
+];
